771dbd9a9562bffda91731b08eab5caac39fab97
BilalAsifB <bilalburney14@gmail.com>
2025-10-08 01:42:31 +0500

    add promise

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import mongoose from "mongoose";
 import connectDB from "./database/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./.env"
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.error("Server error:", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 5000}`);
+    })
+})
+.catch((error) => {
+    console.error("Database connection error:", error);
+    process.exit(1);
+});
