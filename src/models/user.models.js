86f82728db38b479b31975c0254f8ec7678000a9
BilalAsifB <bilalburney14@gmail.com>
2025-10-09 22:18:41 +0500

    fix syntax

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -41,7 +41,7 @@ userSchema.pre("save", async function (next) {
     if (this.isModified("password")) {
         this.password = await bcrypt.hash(
             this.password,
-            salt=10  
+            10
         )
         next()
     }
